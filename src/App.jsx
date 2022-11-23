5853a30c63a96dfb58d0875543aea6d6f33c0c15
matheuskjillim <kjillimlenz@gmail.com>
2022-11-23 10:54:46 -0300

    test

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { Button } from '@chakra-ui/react';
+import React, { useState } from 'react';
+
+export function App() {
+
+  const [dadosDigitados, setDadosDigitados] = useState('')
+  console.log(dadosDigitados)
+
+  function Limpa(ev) {
+    ev.preventDefault();
+    dadosDigitados.push('');
+  }
+
+
+  return (
+    <div >
+      <h1>teste</h1>
+
+      <div style={{ width: '80vw', height: '80vh' }}>
+
+        <input
+          type="text"
+          value={dadosDigitados}
+          onChange={(e) => setDadosDigitados(e.target.value)} />
+
+        {/* <br /> */}
+
+        <Button onClick={limpa} type='reset'>limpa</Button>
+
+      </div>
+    </div>
+  )
+}
